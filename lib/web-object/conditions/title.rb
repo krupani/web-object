68534c2de9d071be7abeaf9d36a86525d9743884
Kaushal Rupani <kushrupani@live.com>
2017-05-22 15:48:27 +0400

    Adding method explanations

--- lib/web-object/conditions/title.rb
+++ lib/web-object/conditions/title.rb
@@ -1,5 +1,10 @@
 module WebConditions
 
+  # @method title_to_match
+  # @param [title] -- expected title to match the actual title of the web page
+  # @return [Boolean true] -- if expected title matches the actual title on the web page
+  # @return [Boolean false] -- if expected title does not match the actual title on the web page
+
   def title_to_match(title)
     actual = @driver.title
     if actual == title
@@ -9,6 +14,10 @@ module WebConditions
     end
   end
 
+  # @method title_to_match
+  # @param [title] -- expected title to match the actual title of the  web page
+  # @return [Boolean true] -- if expected title matches the actual title on the web page
+  # @return [Boolean false] -- if expected title does not match the actual title on the web page
 
   def title_to_include(title)
     actual = @driver.title
--- lib/web-object/conditions/url.rb
+++ lib/web-object/conditions/url.rb
@@ -1,5 +1,10 @@
 module WebConditions
 
+  # @method url_to_match
+  # @param [url] -- expected url to match the actual url on the browser
+  # @return [Boolean true] -- if expected url matches the actual url on the browser
+  # @return [Boolean false] -- if expected url does not match the actual url on the browser
+
   def url_to_match(url)
     actual = @driver.current_url
     if actual == url
@@ -9,6 +14,10 @@ module WebConditions
     end
   end
 
+  # @method url_to_match?
+  # @param [url] -- part of the expected url to be present in actual url in the browser
+  # @return [Boolean true] -- if alert is present on page
+  # @return [Boolean false] -- if alert is absent from page
 
   def url_to_include(url)
     actual = @driver.current_url
